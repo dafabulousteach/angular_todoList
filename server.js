16366cf8fc18f76dfbbf6271f51e839f9ff8bb9f
Kim Merino <dafabulousteach@gmail.com>
2015-11-11 12:32:43 -0800

    adds API routes

--- server.js
+++ server.js
@@ -1,18 +1,90 @@
 "use strict"
 
 var express = require('express');
-var app = express();
-var path = require('path');
-var bodyParser = require('body-parser');
-var port = process.env.PORT || 7575;
-var router = require('./routes.js');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-app.set('view engine', 'ejs');
-// app.use(express.static('client'));
-app.use('/', router);
+var app = express(); // our app using express
+var mongoose = require('mongoose'); // mongoose for mongodb
+var morgan =require('morgan'); // log requests to the console
+var bodyParser = require('body-parser'); // pulls info from HTML POST
+var methodOverride = require('method-override'); // simulate DELETE and PUT
 
-var server = app.listen(port, function(){
-  var host = server.address().address;
-  console.log('Listening at http://%s:%s -- %s', host, port);
+// ================================== //
+//          CONNECT TO MONGODB        //
+// ================================== //
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function(){
+//   var Todo = mongoose.model('Todo', {
+//     text : String
+//   });
+//   console.log('db connection has been created');
+// });
+mongoose.connect('mongodb://admin:password@ds053784.mongolab.com:53784/todo-list');
+
+// ================================== //
+//           SERVER CONFIG            //
+// ================================== //
+app.use(express.static(__dirname + '/public')); // sets static file location
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+app.use(methodOverride());
+// ================================== //
+//              MODELS                //
+// ================================== //
+var Todo = mongoose.model('Todo', {
+    text : String
 });
+
+// ================================== //
+//              ROUTES                //
+// ================================== //
+app.get('/api/todos', function(req, res){
+  Todo.find(function(err, todos){
+    if(err){
+      res.send(err);
+    }
+    res.json(todos);
+  });
+});
+
+app.post('/api/todos', function(req, res){
+  console.log(req.params.todo_id);
+  Todo.create({
+    text:req.body.text,
+    done: false
+  }, function(err, todo){
+    if(err){
+      res.send(err);
+    }
+    Todo.find(function(err, todos){
+      if(err){
+        res.send(err);
+      }
+      res.json(todos);
+    });
+  });
+});
+
+app.delete('/api/todos', function(req, res){
+  Todo.remove({
+    _id : req.params.todo_id
+  }, function(err, todo){
+      if(err){
+        res.send(err);
+      }
+      Todo.find(function(err, todos){
+        if(err){
+          res.send(err);
+        }
+        res.json(todos);
+      });
+  });
+});
+
+// ================================== //
+//       LOAD SINGLE VIEW FILE        //
+// ================================== //
+
+app.listen(8080);
+console.log('App listening on port 8080');
